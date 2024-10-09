6192c5ff47d5eb5148ed06faa62e5bba4beb21b1
Rodrygo Andrade <rodrigoandrade311@gmail.com>
2024-10-09 10:39:19 -0300

    final images adjustment

--- src/data/data.js
+++ src/data/data.js
@@ -1,31 +1,91 @@
 let data = [
   {
     "01d": [
-      ".././img/biking.gif",
-      ".././img/swimming.gif",
-      ".././img/walking.gif",
+      "../../img/biking-comp.gif",
+      "../../img/swimming-comp.gif",
+      "../../img/walking-comp.gif",
     ],
-    "01n": [".././img/Cinema.gif", ".././img/pizza.gif", ".././img/puzzle.gif"],
+    "01n": [
+      "../../img/Cinema-comp.gif",
+      "../../img/pizza-comp.gif",
+      "../../img/puzzle-comp.gif",
+    ],
     "02d": [
-      ".././img/biking.gif",
-      ".././img/swimming.gif",
-      ".././img/walking.gif",
+      "../../img/biking-comp.gif",
+      "../../img/swimming-comp.gif",
+      "../../img/walking-comp.gif",
     ],
-    "02n": [".././img/Cinema.gif", ".././img/pizza.gif", ".././img/puzzle.gif"],
-    "03d": [".././img/biking.gif", ".././img/pets.gif", ".././img/walking.gif"],
-    "03n": [".././img/Cinema.gif", ".././img/pizza.gif", ".././img/puzzle.gif"],
-    "04d": [".././img/pushups.gif", ".././img/pets.gif", ".././img/book.gif"],
-    "04n": [".././img/pets.gif", ".././img/pizza.gif", ".././img/puzzle.gif"],
-    "09d": [".././img/pushups.gif", ".././img/song.gif", ".././img/book.gif"],
-    "09n": [".././img/pets.gif", ".././img/tea.gif", ".././img/puzzle.gif"],
-    "10d": [".././img/pushups.gif", ".././img/pets.gif", ".././img/book.gif"],
-    "10n": [".././img/puzzle.gif", ".././img/tea.gif", ".././img/book.gif"],
-    "11d": [".././img/pushups.gif", ".././img/tea.gif", ".././img/book.gif"],
-    "11n": [".././img/puzzle.gif", ".././img/tea.gif", ".././img/song.gif"],
-    "13d": [".././img/sofa.gif", ".././img/tea.gif", ".././img/book.gif"],
-    "13n": [".././img/puzzle.gif", ".././img/tea.gif", ".././img/song.gif"],
-    "50d": [".././img/sofa.gif", ".././img/tea.gif", ".././img/pets.gif"],
-    "50n": [".././img/puzzle.gif", ".././img/tea.gif", ".././img/song.gif"],
+    "02n": [
+      "../../img/Cinema-comp.gif",
+      "../../img/pizza-comp.gif",
+      "../../img/puzzle-comp.gif",
+    ],
+    "03d": [
+      "../../img/biking-comp.gif",
+      "../../img/pets-comp.gif",
+      "../../img/walking-comp.gif",
+    ],
+    "03n": [
+      "../../img/Cinema-comp.gif",
+      "../../img/pizza-comp.gif",
+      "../../img/puzzle-comp.gif",
+    ],
+    "04d": [
+      "../../img/pushups-comp.gif",
+      "../../img/pets-comp.gif",
+      "../../img/book-comp.gif",
+    ],
+    "04n": [
+      "../../img/pets-comp.gif",
+      "../../img/pizza-comp.gif",
+      "../../img/puzzle-comp.gif",
+    ],
+    "09d": [
+      "../../img/pushups-comp.gif",
+      "../../img/song-comp.gif",
+      "../../img/book-comp.gif",
+    ],
+    "09n": [
+      "../../img/pets-comp.gif",
+      "../../img/tea-comp.gif",
+      "../../img/puzzle-comp.gif",
+    ],
+    "10d": [
+      "../../img/pushups-comp.gif",
+      "../../img/pets-comp.gif",
+      "../../img/book-comp.gif",
+    ],
+    "10n": [
+      "../../img/puzzle-comp.gif",
+      "../../img/tea-comp.gif",
+      "../../img/book-comp.gif",
+    ],
+    "11d": [
+      "../../img/pushups-comp.gif",
+      "../../img/tea-comp.gif",
+      "../../img/book-comp.gif",
+    ],
+    "11n": [
+      "../../img/puzzle-comp.gif",
+      "../../img/tea-comp.gif",
+      "../../img/song-comp.gif",
+    ],
+    "13d": [
+      "../../img/sofa-comp.gif",
+      "../../img/tea-comp.gif",
+      "../../img/book-comp.gif",
+    ],
+    "13n": [
+      "../../img/puzzle-comp.gif",
+      "../../img/tea-comp.gif",
+      "../../img/sofa-comp.gif",
+    ],
+    "50d": [".././img/sofa.gif", ".././img/tea.gif", "../../img/pets-comp.gif"],
+    "50n": [
+      "../../img/puzzle-comp.gif",
+      "../../img/tea-comp.gif",
+      "../../img/song-comp.gif",
+    ],
   },
 ];
 
